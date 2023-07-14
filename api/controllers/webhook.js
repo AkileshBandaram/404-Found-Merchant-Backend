51020244e653370128f6da4791aa1aef740f61ce
Abhinay Guduri <guduriabhinay@gmail.com>
2023-07-13 20:58:15 -0400

    Encryption Added

--- api/controllers/webhook.js
+++ api/controllers/webhook.js
@@ -1,12 +1,13 @@
 const User = require("../models/users")
+const CryptoJS = require('crypto-js');
 
 exports.savetoken = (req,res,next)=>{
     if(req.body.api_key=="API_e453222f-9e44-48c6-b22b-fe5394995bd6"){
         User.scan("user_email").eq(req.body.user_email).exec().then((users)=>{
             if(users.count>0){
-                console.log(req.body)
                 const user = users[0];
-                User.update({user_id:user.user_id, user_token:req.body.user_token}).then(()=>{
+                const encryptedToken = CryptoJS.AES.encrypt(req.body.user_token, 'A7mBSxZrjCYZObWd1ziU5wYqm5KyFUqK').toString();
+                User.update({user_id:user.user_id, user_token:encryptedToken}).then(()=>{
                     res.status(200).json({status:1, message:"Token Saved"})
                 }).catch((err)=>{
                     res.status(500).json({status:0,message:"Something went wrong"})
